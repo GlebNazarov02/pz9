01c8b7be72c2c63938618c09d3373d50fd06ba3b
Gelix1470 <gleb.nazarov.1982@mail,ru>
2022-12-19 21:29:55 +0300

    newb

---
+++ pz9/1.0/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
+
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    NumberCounter (number - 1);
+    Console.Write($"{number}"); 
+}
+
+NumberCounter(number);
---
+++ pz9/1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1 натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 натуральное число больше 1:");
+int number1 = int.Parse(Console.ReadLine());
+
+void NumberCounter (int number,int number1)
+{
+    if (number1 + 1 < number) Console.Write("нет");
+    if (number == number1+1) return;
+    Console.Write($"{number} "); 
+    NumberCounter (number + 1,number1);
+}
+
+NumberCounter(number,number1);
